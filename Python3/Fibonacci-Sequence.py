dd426c9801d5be791648778278be2cff099c4f78
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2022-11-20 14:34:46 +1100

    Fibonacci Sequence in Python3

---
+++ Python3/Fibonacci-Sequence.py
@@ -0,0 +1,12 @@
+#Fibonacci Sequence coding interview question in Python3
+#by Freya-The-Goddess
+
+def fibonacci(n):
+    output = [0, 1, 1]
+    for _ in range(3,n):
+        output.append(output[-1] + output[-2]) #append sum of last two values
+    return output
+
+fib_arr = fibonacci(50)
+for value in fib_arr: #print each value of array
+    print(value)
