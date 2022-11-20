9a525d9d8872ee50ee404539a2e6d1dfb20471f9
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2022-11-20 15:41:48 +1100

    Fibonacci Sequence in Kotlin

---
+++ Kotlin/Fobonacci.kt
@@ -0,0 +1,18 @@
+//Fibonacci Sequence coding interview question in Kotlin
+//by Freya-The-Goddess
+
+fun fibonacci(n: Int): MutableList<Int> {
+    val output = mutableListOf(0, 1, 1)
+    (3 until n).forEach { _ ->
+        val lastIndex = output.lastIndex
+        output.add(output[lastIndex] + output[lastIndex-1]) //append sum of last two values
+    }
+    return output
+}
+
+fun main() {
+    val fibList = fibonacci(25)
+    fibList.forEach { value -> //print each value of array
+        println(value)
+    }
+}
